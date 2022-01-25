5eb15e5d0c526dd523d4567b732fd69a2e44e2ee
Malik D <70532673+s11069234@users.noreply.github.com>
2022-01-25 09:15:25 -0500

    v0.7
    Checked for KEYUP on arrow keys / WASD to stop moving.
    
    Created 'teleport' button using x.

--- collision_detection.py
+++ collision_detection.py
@@ -1,5 +1,6 @@
-# PyGame Collision Detection Practice, Malik Durgan, January 25, 2022, 9:07am, v0.6
+# PyGame Collision Detection Practice, Malik Durgan, January 25, 2022, 9:14am, v0.7
 
+from curses import KEY_RIGHT
 import pygame, sys, random
 from pygame.locals import *
 
@@ -57,3 +58,19 @@ while True:
             if event.key == K_DOWN or event.key == K_s:
                 moveUp = False
                 moveDown = True
+        if event.type == KEYUP:
+            if event.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+            # Check to see if the player has stopped moving.
+            if event.key == K_LEFT or event.key == K_a:
+                moveLeft = False
+            if event.key == K_RIGHT or event.key == K_d:
+                moveRight = False
+            if event.key == K_UP or event.key == K_w:
+                moveUp = False
+            if event.key == K_DOWN or event.key == K_s:
+                moveDown = False
+            if event.key == K_x: # Use x to teleport the player.
+                player.top = random.randint(0, WINDOWHEIGHT - player.height)
+                player.left = random.randint(0, WINDOWWIDTH - player.width)
